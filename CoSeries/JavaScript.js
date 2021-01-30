1f0d63b959d5e997a644543be9bbd44a602f42af
Vinicius Teixeira <viniciusteixeiravini16@gmail.com>
2021-01-30 00:34:42 -0300

    Screen de informações adicionais criadas

--- CoSeries/JavaScript.js
+++ CoSeries/JavaScript.js
@@ -1,3 +1,19 @@
 document.querySelector(".bars").addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
 })
+
+document.querySelector(".details-friends").addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu-friends");
+})
+
+ document.querySelector(".close-friends").addEventListener("click", function(){
+     document.querySelector(".container").classList.toggle("show-menu-friends");
+})
+
+document.querySelector(".details-brooklyn-nine-nine").addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu-brooklyn");
+})
+
+document.querySelector(".close-brooklyn").addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu-brooklyn");
+})
