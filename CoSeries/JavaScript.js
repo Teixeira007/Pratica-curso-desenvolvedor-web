1dea495b04bddb0edd6842915e3911ec227ebef0
Vinicius Teixeira <viniciusteixeiravini16@gmail.com>
2021-01-30 20:24:41 -0300

    Screen de informações responsivas

--- CoSeries/JavaScript.js
+++ CoSeries/JavaScript.js
@@ -17,3 +17,19 @@ document.querySelector(".details-brooklyn-nine-nine").addEventListener("click", 
 document.querySelector(".close-brooklyn").addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu-brooklyn");
 })
+
+document.querySelector(".details-the-flash").addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu-the-flash");
+})
+
+document.querySelector(".close-the-flash").addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu-the-flash");
+})
+
+document.querySelector(".details-arrow").addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu-arrow");
+})
+
+document.querySelector(".close-arrow").addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu-arrow");
+})
